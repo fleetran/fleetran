ed009889733f746f352f202eacdb9bed6ec7c0ef
srbre21 <51722679+srbre21@users.noreply.github.com>
2019-06-11 16:42:49 -0400

    Add files via upload

---
+++ footer-distributed-with-address-and-phones.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, address, phone, icons" />
+
+	<title>Footer With Address And Phones</title>
+
+	<link rel="stylesheet" href="css/demo.css">
+	<link rel="stylesheet" href="css/footer-distributed-with-address-and-phones.css">
+	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+	<link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+
+</head>
+
+	<body>
+
+		<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<a href="portal.php"><img src="img/logomin.png" /></a><h3>Fleetran</h3>
+
+				<p class="footer-links">
+					<a href="#">Inicio</a>					·
+					<a href="#">¿Cómo funciona?</a>					·
+					<a href="#">Planes</a>					·
+					<a href="#">¿Quienes somos?</a>					·
+					<a href="#">Contacto</a>
+				</p>
+
+				<p class="footer-company-name">Fleetran &copy; <?php echo date("Y");?></p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Nelson Pereira #345</span>Rancagua, Chile.</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+56 9 7652 0000</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:soporte@fleetran.cl">soporte@fleetran.cl</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>Nuestra empresa</span>
+					El equipo de Fleetran está enfocado en el desarrollo de una solución informática, para administradores de flotas de vehículos. El objetivo es facilitar la gestión, tener un mejor control de los activos y obtener información en cualquier lugar cuando se necesite.
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-instagram"></i></a>
+					<a href="#"><i class="fa fa-youtube"></i></a>
+
+				</div>
+
+			</div>
+
+		</footer>
+
+	</body>
+
+</html>
---
+++ salir.php
@@ -0,0 +1,14 @@
+﻿<?php
+	//ELIMINA LA SESIÓN, ENVIA UN MENSAJE DE CERRAR SESION Y REDIRIGE AL INDEX
+	session_start();
+	session_destroy();
+	echo "<script language='javascript'>alert('Cerrando sesión');</script>";
+?>
+<html>
+<head>
+<title>Saliendo...</title>
+</head>
+<body>
+<script language="javascript">location.href = "index.php";</script>
+</body>
+</html>
