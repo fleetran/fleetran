eaf473fdc6fbded259bda9af26e32ac7f14f5602
srbre21 <srbre21@gmail.com>
2019-06-14 18:18:29 -0400

    Signed-off-by: srbre21 <srbre21@gmail.com>

--- extras/script fleetran.sql
+++ extras/script fleetran.sql
@@ -1,25 +1,61 @@
 create database fleetran;
 use fleetran;
+
+create table plan(
+id_plan int auto_increment primary key,
+actividad_plan varchar(100),
+flota_plan varchar(100),
+monto_plan int
+);
+
+insert into plan value(null,"Camiones +2 ejes","1 a 5 vehiculos",27000);
+insert into plan value(null,"Camiones +2 ejes","6 a 20 vehiculos",30000);
+insert into plan value(null,"Camiones +2 ejes","21 a 100 vehiculos",36000);
+insert into plan value(null,"Vehiculos de construccion","1 a 5 vehiculos",20250);
+insert into plan value(null,"Vehiculos de construccion","6 a 20 vehiculos",22500);
+insert into plan value(null,"Vehiculos de construccion","21 a 100 vehiculos",27000);
+insert into plan value(null,"Maquiarias Agricolas","1 a 5 vehiculos",17550);
+insert into plan value(null,"Maquiarias Agricolas","6 a 20 vehiculos",19500);
+insert into plan value(null,"Maquiarias Agricolas","21 a 100 vehiculos",23400);
+insert into plan value(null,"Furgones - Camiones 3/4","1 a 5 vehiculos",20250);
+insert into plan value(null,"Furgones - Camiones 3/4","6 a 20 vehiculos",22500);
+insert into plan value(null,"Furgones - Camiones 3/4","21 a 100 vehiculos",27000);
+insert into plan value(null,"Taxis - Colectivos - Radiotaxis","1 a 5 vehiculos",20250);
+insert into plan value(null,"Taxis - Colectivos - Radiotaxis","6 a 20 vehiculos",22500);
+insert into plan value(null,"Taxis - Colectivos - Radiotaxis","21 a 100 vehiculos",27000);
+insert into plan value(null,"Rent-a-car","1 a 5 vehiculos",20250);
+insert into plan value(null,"Rent-a-car","6 a 20 vehiculos",22500);
+insert into plan value(null,"Rent-a-car","21 a 100 vehiculos",27000);
+insert into plan value(null,"Motos","1 a 5 vehiculos",10800);
+insert into plan value(null,"Motos","6 a 20 vehiculos",12000);
+insert into plan value(null,"Motos","21 a 100 vehiculos",14400);		
+
 create table usuario(
-email_user varchar(50)  PRIMARY KEY,
+rut_user varchar(10)  PRIMARY KEY,
 nombre_user varchar(30),
-apellido_user varchar(30),
-rut_user varchar(10),
-empresa_user varchar(30),
-password_user varchar(100)
+email_user varchar(50),
+id_plan int,
+password_user varchar(100),
+foreign key plan(id_plan) references plan(id_plan)
 );
 
 create table licencias(
 id_licencia int auto_increment primary key,
-email_user varchar(50),
-descripcion_licencia varchar(100),
+rut_user varchar(50),
 inicio_licencia varchar(10),
-diasrestantes_licencia int,
 monto_licencia int,
-foreign key usuario(email_user) references usuario(email_user)
+foreign key usuario(rut_user) references usuario(rut_user)
 );
+insert into licencias value(null,'19359735-1','14/06/2019','25000');
+
+
 -- Usuario: alvi@inacap.cl pass: 123456
 -- Usuario: mati@inacap.cl pass: 123456
-insert into usuario values('alvi@inacap.cl','alvaro','urzua','19359735-1','alvi corp','e10adc3949ba59abbe56e057f20f883e');
-insert into usuario values('mati@inacap.cl','alvaro','urzua','19359735-1','mati corp','e10adc3949ba59abbe56e057f20f883e');
-insert into licencias value(null,'alvi@inacap.cl','PLAN PRO - 30 DIAS','28-05-2019',30,100000);
+insert into usuario values('19359735-1','alvaro','alvi@inacap.cl',7,'e10adc3949ba59abbe56e057f20f883e');
+insert into licencias value(null,'19359735-1','14/06/2019',100000);
+
+select * from licencias;
+select * from usuario;
+select * from plan;
+drop database fleetran;
+select rut_user,nombre_user,email_user,plan.actividad_plan,plan.flota_plan,password_user from usuario,plan where usuario.id_plan=plan.id_plan;
